00dfdf509507ce8699f722877a01a242e1919eba
Yulia  Demir <Nightelfin91@gmail.com>
2025-05-19 11:21:51 +0300

    fix: change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { httpServer } from "./src/http_server/index.js";
 import { startWebSocketServer } from "./dist/websocket.js";
 
-const HTTP_PORT = 3000;
+const HTTP_PORT = 8181;
 
 httpServer.listen(HTTP_PORT, () => {
     startWebSocketServer(httpServer);
